bf2b78350f782cfe3a25386a6f0d35c57e290a97
Jop van de Hoef <jopvdhoef@gmail.com>
2024-03-26 16:50:15 +0100

    initial Sprint structure

---
+++ WTAcademy/src/main/java/nl/workingtalent/WTAcademy/WtAcademyApplication.java
@@ -0,0 +1,13 @@
+package nl.workingtalent.WTAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtAcademyApplication.class, args);
+	}
+
+}
---
+++ WTAcademy/src/test/java/nl/workingtalent/WTAcademy/WtAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.workingtalent.WTAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
