3e664b18da80b449792fa0d01457f76a55d4eaf3
Larsvandijk <larsvandijk1999@gmail.com>
2024-04-02 12:56:53 +0200

    Update Book.java

--- src/main/java/nl/workingtalent/wtacademy/book/Book.java
+++ src/main/java/nl/workingtalent/wtacademy/book/Book.java
@@ -1,9 +1,7 @@
 package nl.workingtalent.wtacademy.book;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
