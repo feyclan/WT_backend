0f7edbb61bee91be888090f739868467c95f4ea5
Larsvandijk <larsvandijk1999@gmail.com>
2024-04-04 15:20:24 +0200

    removed unused imports

--- src/main/java/nl/workingtalent/wtacademy/bookcopy/BookCopyController.java
+++ src/main/java/nl/workingtalent/wtacademy/bookcopy/BookCopyController.java
@@ -1,6 +1,5 @@
 package nl.workingtalent.wtacademy.bookcopy;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.workingtalent.wtacademy.book.Book;
