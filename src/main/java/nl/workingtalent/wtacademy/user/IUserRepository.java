dd5181a502d943adcb0287b4da9633a462dc92a4
Ilias <ilias.azagagh@icloud.com>
2024-04-12 14:46:59 +0200

    Cleaned up code

--- src/main/java/nl/workingtalent/wtacademy/user/IUserRepository.java
+++ src/main/java/nl/workingtalent/wtacademy/user/IUserRepository.java
@@ -1,16 +1,10 @@
 package nl.workingtalent.wtacademy.user;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import nl.workingtalent.wtacademy.book.Book;
-
 public interface IUserRepository extends JpaRepository<User, Long> {
-
-	Optional<User> findUserByEmail(String user);
 
 	// Derived query
 	List<User> findAll(Specification<User> spec);
