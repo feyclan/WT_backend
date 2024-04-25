b0608df1002fe1d92ac802fde5fb53db307faa44
Larsvandijk <larsvandijk1999@gmail.com>
2024-04-25 10:14:50 +0200

    Update CategoryService.java

--- src/main/java/nl/workingtalent/wtacademy/category/CategoryService.java
+++ src/main/java/nl/workingtalent/wtacademy/category/CategoryService.java
@@ -10,12 +10,27 @@ public class CategoryService {
 
 	@Autowired
 	private ICategoryRepository repository;
-	
+
+	/**
+	 * This method is used to add a new category to the database.
+	 * Before adding, it checks if the category already exists in the database.
+	 * If the category does not exist, it creates a new Category object, sets the category name, and saves it to the database.
+	 *
+	 * @param category The name of the category to be added.
+	 */
 	public void addCategory(String category) {
+		// Fetch the category from the database
 		Optional<Category> dbCategory = getCategoryByName(category);
+
+		// Check if the category already exists
 		if (dbCategory.isEmpty()) {
+			// If not, create a new Category object
 			Category newCategory = new Category();
+
+			// Set the category name
 			newCategory.setCategory(category);
+
+			// Save the new category to the database
 			repository.save(newCategory);
 		}
 	}
